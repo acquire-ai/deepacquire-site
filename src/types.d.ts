199eb32f1d8a0681b09414ae13273210660409e2
leixin <chjgreen@hotmail.com>
2025-11-03 13:05:29 +0800

    add to edge

--- src/types.d.ts
+++ src/types.d.ts
@@ -276,6 +276,7 @@ export interface Content extends Omit<Headline, 'classes'>, Widget {
   isReversed?: boolean;
   isAfterContent?: boolean;
   callToAction?: CallToAction;
+  callToAction2?: CallToAction;
 }
 
 export interface Contact extends Omit<Headline, 'classes'>, Form, Widget {}
